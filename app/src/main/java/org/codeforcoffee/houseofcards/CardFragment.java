fba29b4531bd16e9c478203e685d65f78dd9deb0
code-for-coffee <jtraverjr@gmail.com>
2016-07-06 14:35:35 -0500

    woot

--- app/src/main/java/org/codeforcoffee/houseofcards/CardFragment.java
+++ app/src/main/java/org/codeforcoffee/houseofcards/CardFragment.java
@@ -15,13 +15,15 @@ public class CardFragment extends android.support.v4.app.Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
 
+        Bundle argumentsFromActivity = this.getArguments(); // get my arguments from my activity
+
         View cardFragment = inflater.inflate(R.layout.card_template, container, false);
 
         TextView header = (TextView) cardFragment.findViewById(R.id.card_header);
         TextView body = (TextView) cardFragment.findViewById(R.id.card_body);
 
-        header.setText("My first card!");
-        body.setText("blah blue dee da bu die i'm blue i'm de");
+        header.setText(argumentsFromActivity.getString("HEAD"));
+        body.setText(argumentsFromActivity.getString("BODY"));
 
         return cardFragment;
     }
--- app/src/main/java/org/codeforcoffee/houseofcards/ScrollingActivity.java
+++ app/src/main/java/org/codeforcoffee/houseofcards/ScrollingActivity.java
@@ -1,39 +1,76 @@
 package org.codeforcoffee.houseofcards;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ScrollingActivity extends AppCompatActivity {
 
-    @Override
+    AlertDialog.Builder mAlert;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mAlert = new AlertDialog.Builder(this);     // new alert dialog w/access to context
+        mAlert.setView(R.layout.dialog_add_new);    // set dialog view to xml file
+
+        // set a positive button :)
+        // and override the OnClickListener
+        mAlert.setPositiveButton(R.string.dialog_add, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(DialogInterface dialog, int which) {
+                Dialog popup = (Dialog) dialog;
+                EditText headerText = (EditText) popup.findViewById(R.id.input_header);
+                EditText bodyText = (EditText) popup.findViewById(R.id.input_body);
+
+                // create a new bundle to send to our fragment
+                Bundle fragmentArguments = new Bundle();
+                fragmentArguments.putString("HEAD", headerText.getText().toString());
+                fragmentArguments.putString("BODY", bodyText.getText().toString());
+                // add our values ^^
 
                 // do the fragment
                 FragmentManager fm = getSupportFragmentManager();
                 FragmentTransaction ft = fm.beginTransaction();
 
                 CardFragment underwoodCard = new CardFragment();
+                underwoodCard.setArguments(fragmentArguments);  // set our bundle from above
+                // to our fragment's arguments
+
                 ft.add(R.id.card_holder, underwoodCard);
 
                 ft.commit();
                 // commit my fragment
+            }
+        });
+        mAlert.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(ScrollingActivity.this, "User cancelled adding", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAlert.show(); // show our dialog :)
 
                 Snackbar.make(view, "AHAAAHAAHA CARDSSSSS", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
